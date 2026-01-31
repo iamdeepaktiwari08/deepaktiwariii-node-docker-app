2c354119214884d36ca86400e23d749d8b5dcd7d
Deepak Tiwari <imdeepaktiwari08@gmail.com>
2026-01-31 20:09:46 +0530

    repo

---
+++ main.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8000;
+app.get('/', (req, res) => {
+    return res.json({ message: 'Hello, World!' });
+});
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+}); 
